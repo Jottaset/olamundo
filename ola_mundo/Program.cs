5c9e6b8cc886d78371634784baaa4fb6aad7431b
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-01 18:13:08 -0400

    Commit Inicial

---
+++ ola_mundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+            Console.WriteLine("Hello Brasil!\n");
+            Console.WriteLine("Hello Amazonas!\n");
+            Console.WriteLine("Hello Manaus!\n");
+            Console.WriteLine("Hello Fucapi!\n");
+            //Comentando
+            //Outro comentario
+            Console.Write("teste2!\t");
+            Console.Write("teste3!");
+        }
+    }
+}
